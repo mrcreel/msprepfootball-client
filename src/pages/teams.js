55e107d1a125251702c821b91a08976f491e7cf1
Michael R Creel <michaelrcreel@gmail.com>
2019-08-03 12:02:40 -0500

    Init commit

---
+++ src/pages/teams.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class teams extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Teams page</h1>
+      </div>
+    )
+  }
+}
+
+export default teams
